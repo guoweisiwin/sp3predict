9a45c6e350e7ae5d08c5defdd939a25124cf7c6c
Philip Fowler <philipwfowler@gmail.com>
2019-12-11 14:03:19 +0000

    setup basic repo

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from piezo import __version__
+
+setup(
+    name='sp3predict',
+    version=__version__,
+    author='Philip W Fowler',
+    author_email="philip.fowler@ndm.ox.ac.uk",
+    description="Antibiotic suscetibility predictions from a VCF file and a TB AMR catalogue.",
+    url="https://github.com/philipwfowler/sp3predict",
+    packages=['sp3predict'],
+    package_data={''},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"  ],
+    python_requires='>=3.5',
+    install_requires=[
+        "pandas >= 0.23.1"
+    ],
+    license='University of Oxford, see LICENSE.md',
+    scripts=['bin/sp3predict.py'],
+    zip_safe=False
+)

b947a2bcfead3e37d6dae1aa5e178bc8807f1ff3
Philip Fowler <philipwfowler@gmail.com>
2019-12-11 14:17:22 +0000

    added config/ folder with genbank file

--- setup.py
+++ setup.py
@@ -8,14 +8,17 @@ setup(
     author_email="philip.fowler@ndm.ox.ac.uk",
     description="Antibiotic suscetibility predictions from a VCF file and a TB AMR catalogue.",
     url="https://github.com/philipwfowler/sp3predict",
-    packages=['sp3predict'],
-    package_data={''},
+    package_data={'config/'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"  ],
     python_requires='>=3.5',
     install_requires=[
-        "pandas >= 0.23.1"
+        "pandas >= 0.23.1",\
+        "gumpy",\
+        "piezo",\
+        "tqdm",\
+        "numpy"
     ],
     license='University of Oxford, see LICENSE.md',
     scripts=['bin/sp3predict.py'],
